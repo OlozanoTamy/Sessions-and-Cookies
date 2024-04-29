c5188f777e853b82987c83b5da5dffa6184ef8b3
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-04-29 14:05:03 -0500

    Se implementa el uso de passport para le verificacion de identidad al momento de ingresar a la app

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import bcrypt from "bcrypt";
 //Se importa desde session
 import session from "express-session";
 import passport from "passport";
+//se importa Strategy que viene de passport-local
+import { Strategy } from "passport-local";
 
 const app = express();
 const port = 3000;
@@ -13,11 +15,15 @@ const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//usa el middleware de session
+//usa el middleware de session debe instanciarse ante de passport
 app.use(session({
   secret: "TOPSECRETWORD",
   resave: false,
   saveUninitialized: true,
+  //cookie permite establecer el tiempo que va a estar activa la cookie
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+  }
 }));
 
 app.use(passport.initialize());
@@ -26,7 +32,7 @@ app.use(passport.session());
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "secrets",
+  database: "usuarios",
   password: "UNCeax35",
   port: 5433,
 });
@@ -40,13 +46,15 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+
+//esta peticion get solicita si existe una cookies que diga que la sesion esta activa y permite ver la pagina secrets.ejs
 app.get("/secrets", (req, res) => {
   if (req.isAuthenticated()) {
     res.render("secrets.ejs");
   } else {
     res.redirect("/login");
   }
-})
+});
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
@@ -65,16 +73,23 @@ app.post("/register", async (req, res) => {
       res.send("Email already exists. Try logging in.");
     } else {
       //hashing the password and saving it in the database
+      //este paso usa el modulo bcrypt para hassiar la contraseña y le añade la cantidad de veces salt y devuelve un callback ya sea con un error o un hash
       bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
           console.error("Error hashing password:", err);
         } else {
           console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("secrets.ejs");
+
+          //esto permite redireccionar al usurio recien creado en y envia a la persona a la pagina secrets         
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log(err);
+            res.redirect("/secrets");
+          });
         }
       });
     }
@@ -83,35 +98,49 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
 
+//passport validad si la estrategia local es existosa o falla
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secrets",
+  failureRedirect: "/login",
+}))
+//passport use una estrategia
+//Usando la funcion verifi accede a el username y password  para comparar con el hasseado que esta en la base de datos
+passport.use(new Strategy(async function verify(username, password, cb) {
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
+      username,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          return cb(err);
         } else {
           if (result) {
-            res.render("secrets.ejs");
+            return cb(null, user);
           } else {
-            res.send("Incorrect Password");
+            return cb(null, false);
           }
         }
       });
     } else {
-      res.send("User not found");
+      return cb("User not found");
     }
   } catch (err) {
-    console.log(err);
+    return cb(err);
   }
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
 });
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
